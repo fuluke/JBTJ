33492c32e730f9dca35c0cffbac7dafc15349030
Wong Jason <fulukeling@outlook.com>
2022-10-24 17:29:36 +0800

    Initial commit

---
+++ src/main/java/wa/sdd/jbtj/JbtjApplication.java
@@ -0,0 +1,13 @@
+package wa.sdd.jbtj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbtjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbtjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wa/sdd/jbtj/JbtjApplicationTests.java
@@ -0,0 +1,13 @@
+package wa.sdd.jbtj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbtjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
